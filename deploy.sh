19578e2c9e20ef8bd989d080f56f20b1c97db81a
Seongmo Jeon <oeeen3@gmail.com>
2019-12-04 13:07:21 +0900

    feat: 배포 테스트

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,23 @@
 
 DOCKER_APP_NAME=heavenApp
 DOCKER_DB_NAME=heavenDB
+DOCKER_NGINX_NAME=heavenNginx
 
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 EXIST_DB=$(docker-compose -p ${DOCKER_DB_NAME} -f docker-compose.db.yml ps | grep Up)
 
+EXIST_NGINX=$(docker-compose -p ${DOCKER_NGINX_NAME} -f docker-compose.db.yml ps | grep Up)
+
+
 if [ -z "$EXIST_DB" ]; then
     echo "DB setting"
     docker-compose -p ${DOCKER_DB_NAME} -f docker-compose.db.yml up -d
+fi
+
+if [ -z "$EXIST_NGINX" ]; then
+    echo "NGINX setting"
+    docker-compose -p ${DOCKER_NGINX_NAME} -f docker-compose.nginx.yml up -d
 fi
 
 if [ -z "$EXIST_BLUE" ]; then
